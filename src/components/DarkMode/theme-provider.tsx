3dee8771a5dae7be7cbcf76eeb71cfebeffe6d72
Nitish Chavan <nitishchavan937@gmail.com>
2025-08-19 12:34:18 +0530

    cretae hero apporach section

--- src/components/DarkMode/theme-provider.tsx
+++ src/components/DarkMode/theme-provider.tsx
@@ -1,15 +1,13 @@
 "use client";
 import dynamic from "next/dynamic";
-import { ReactNode } from "react";
 
 const DynamicThemeProvider = dynamic(
   () => import("next-themes").then((mod) => mod.ThemeProvider),
   { ssr: false }
 );
 
-export function ThemeProvider(props: {
-  children: ReactNode;
-  [key: string]: any;
-}) {
+import type { ThemeProviderProps } from "next-themes";
+
+export function ThemeProvider(props: ThemeProviderProps) {
   return <DynamicThemeProvider {...props} />;
 }
